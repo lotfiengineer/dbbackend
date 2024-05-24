32528c81d48733685f7c210b356d9961893d35cf
lotfiengineer <mohammad.lotfi7913@gmail.com>
2024-05-24 14:05:34 +0330

    implemented get of students, professors and departments, and creating a new student

--- dbbackend.js
+++ dbbackend.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const http = require("http");
 const axios = require("axios");
 
-const PORT = 3001;
+const PORT = 3003;
 
 // Define MySQL connection settings
 const connection = mysql.createConnection({
@@ -22,30 +22,98 @@ connection.connect((err) => {
 });
 
 const server = http.createServer((req, res) => {
-  const professorsSqlQuery = "SELECT * FROM instructor";
-  const studentsSqlQuery = "SELECT * FROM student";
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET, DELETE",
+    "Access-Control-Max-Age": 2592000,
+  };
 
   if (req.url === "/professors") {
+    const professorsSqlQuery = "SELECT * FROM instructor";
+
     if (req.method === "GET") {
       connection.query(professorsSqlQuery, (err, results) => {
-        res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, headers);
         res.end(JSON.stringify(results));
       });
-    } else if (req.method === "POST") {
-      // code
     }
   }
 
   if (req.url === "/students") {
+    const studentsSqlQuery = "SELECT * FROM student";
+
     if (req.method === "GET") {
       connection.query(studentsSqlQuery, (err, results) => {
-        res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(results));
+      });
+    }
+
+    if (req.method === "POST") {
+      let body = "";
+
+      // Collect data chunks
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const data = JSON.parse(body);
+          const studentId = data.studentId;
+          const studentName = data.studentName;
+          const departmentName = data.departmentName;
+          const totalCredit = data.totalCredit;
+
+          // Insert data into the database
+          const query =
+            "INSERT INTO student (ID, name, dept_name, tot_cred) VALUES (?, ?, ?, ?)";
+          connection.query(
+            query,
+            [studentId, studentName, departmentName, totalCredit],
+            (err, results) => {
+              if (err) {
+                console.error("Error inserting data into the database:", err);
+                res.writeHead(500, headers);
+                res.end("Internal Server Error");
+                return;
+              }
+
+              res.writeHead(201, headers);
+              res.end("Data received and inserted");
+            }
+          );
+        } catch (error) {
+          res.writeHead(400, { "Content-Type": "text/plain" });
+          res.end("Invalid JSON");
+        }
+      });
+    }
+  }
+
+  if (req.url === "/courses") {
+    const coursesSqlQuery = "SELECT * FROM course";
+
+    if (req.method === "GET") {
+      connection.query(coursesSqlQuery, (err, results) => {
+        res.writeHead(200, headers);
         res.end(JSON.stringify(results));
       });
     }
   }
 
-  // res.writeHead(404, { "Content-Type": "text/plain" });
+  if (req.url === "/departments") {
+    const departmentsSqlQuery = "SELECT * FROM department";
+
+    if (req.method === "GET") {
+      connection.query(departmentsSqlQuery, (err, results) => {
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(results));
+      });
+    }
+  }
+
+  // res.writeHead(404, headers);
   // res.end("Not Found");
 });
 
